ecd45a083c1ac81d94ce0a8aabb0a25e5c42554d
Jack Cherng <jfcherng@gmail.com>
2020-08-01 14:46:27 +0800

    feat: add scripts to auto download partitioned fonts
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ scripts/download_config.py
@@ -0,0 +1,11 @@
+FONT_LANG = "SC"
+
+FONT_STYLES = {
+    "extra_light": "200",
+    "light": "300",
+    "regular": "400",
+    "medium": "500",
+    "semi_bold": "600",
+    "bold": "700",
+    "black": "900",
+}
